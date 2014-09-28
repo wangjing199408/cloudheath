ecaea96a59f2d622b1380a7c886f4f374ba652f1
guanbo <guanbo2002@gmail.com>
2014-09-28 18:02:06 +0800

    Fixed：serialNumber count

--- common/models/task.js
+++ common/models/task.js
@@ -2,7 +2,7 @@ module.exports = function(Task) {
   Task.beforeSave = function (next, task) {
     task.created = new Date()
     task.status = 'settling'
-    Task.findOne({order:'created DESC'}, function (err, lastTask) {
+    Task.findOne({order:'serialNumber DESC'}, function (err, lastTask) {
       task.serialNumber = lastTask.serialNumber+1
       next()
     })
