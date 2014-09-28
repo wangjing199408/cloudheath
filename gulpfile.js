d374752039c3de7e9222694d1b4f31ee4b339c5a
guanbo <guanbo2002@gmail.com>
2014-09-28 14:36:54 +0800

    Add: bower

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,6 @@ var loopbackAngular = require('gulp-loopback-sdk-angular');
 gulp.task('default', function () {
   return gulp.src('./server/server.js')
     .pipe(loopbackAngular({apiUrl:'http://42.121.124.27:3000/api'}))
-    .pipe(rename('lb-services.js'))
-    .pipe(gulp.dest('./client/js'));
+    .pipe(rename('cloudheath.js'))
+    .pipe(gulp.dest('./bower-cloudheath/'));
 });
